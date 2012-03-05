5d69ff9f3f84bbab067b417bd3412218daffc575
Ted Neward <ted@tedneward.com>
2012-03-05 03:53:06 -0800

    Added kata-choice via URL functionality, as well as 'all' list

---
+++ code/js/urlvars.js
@@ -0,0 +1,16 @@
+$.extend({
+  getUrlVars: function(){
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+      hash = hashes[i].split('=');
+      vars.push(hash[0]);
+      vars[hash[0]] = hash[1];
+    }
+    return vars;
+  },
+  getUrlVar: function(name){
+    return $.getUrlVars()[name];
+  }
+});
